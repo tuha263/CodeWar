b127e3ca583a08abf835819a27d50e32f4e60472
Hoàng Anh Tú <tuha263@gmail.com>
2017-04-14 18:25:20 +0700

    iudpate

--- Manufacture/Manufacture.go
+++ Manufacture/Manufacture.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -13,6 +14,12 @@ type Ring struct {
 	a, b, h int
 }
 
+//RingPoiter point to ring
+type RingPoiter struct {
+	value   *int
+	pointer *Ring
+}
+
 const (
 	//MAXN is max of n
 	MAXN = 100000
@@ -21,9 +28,10 @@ const (
 )
 
 var (
-	n    int
-	ring [MAXN + 1]Ring
-	f    [MAXN + 1]int64
+	n, maxab int
+	ring     [MAXN + 1]Ring
+	f        [MAXN + 1]int64
+	pointer  [MAXN*2 + 2]RingPoiter
 )
 
 func main() {
@@ -43,20 +51,36 @@ func main() {
 	}
 
 	QuickSort(0, n-1)
+	Discrete()
 	ring[n] = Ring{0, MAXX + 1, 0}
 
 	f[0] = int64(ring[0].h)
 	for i := 0; i <= n; i++ {
-		var max int64
-		for j := 0; j < i; j++ {
-			if ring[i].b > ring[j].a && max < f[j] {
-				max = f[j]
-			}
-			f[i] = max + int64(ring[i].h)
-		}
+		f[i] = GetMAX(ring[i].b-1) + int64(ring[i].h)
+		UpdateBIT(ring[i].b, f[i])
 	}
 
 	fmt.Println(f[n])
+}
+
+// UpdateBIT update BIT
+func UpdateBIT(i int, value int64) {
+	if i <= n*2 {
+		f[i] = int64(math.Max(float64(value), float64(f[i])))
+		UpdateBIT(i+i&(-i), value)
+	}
+}
+
+// GetMAX get max form 0..i
+func GetMAX(i int) int64 {
+	fmt.Println(i)
+	if i == 0 {
+		return 0
+	}
+	if i-i&(-i) == 0 {
+		return f[i]
+	}
+	return int64(math.Max(float64(f[i]), float64(GetMAX(i-i&(-i)))))
 }
 
 //QuickSort sort ring list
@@ -87,6 +111,56 @@ func QuickSort(left, right int) {
 
 		if right > l {
 			QuickSort(l, right)
+		}
+	}
+}
+
+//Discrete discrete a and b of ring
+func Discrete() {
+	for i := 0; i < n*2; i++ {
+		pointer[i*2].pointer = &ring[i]
+		pointer[i*2].value = &ring[i].a
+
+		pointer[i*2+1].pointer = &ring[i]
+		pointer[i*2+1].value = &ring[i].b
+	}
+
+	QuickSortPoint(0, n*2-1)
+
+	for i := 0; i < n*2; i++ {
+		*pointer[i].value = i + 1
+	}
+}
+
+//QuickSortPoint sort ring point list
+func QuickSortPoint(left, right int) {
+	l := left
+	r := right
+	m := (left + right) / 2
+	pivot := *pointer[m].value
+	for l < r {
+		for *pointer[l].value < pivot || (*pointer[l].value == pivot && l%2 == 1 && m%2 == 0) {
+			l++
+		}
+
+		for *pointer[r].value > pivot || (*pointer[r].value == pivot && r%2 == 0 && m%2 == 1) {
+			r--
+		}
+
+		if l <= r {
+			temp := pointer[l]
+			pointer[l] = pointer[r]
+			pointer[r] = temp
+			l++
+			r--
+		}
+
+		if left < r {
+			QuickSortPoint(left, r)
+		}
+
+		if right > l {
+			QuickSortPoint(l, right)
 		}
 	}
 
--- Test/Test.go
+++ Test/Test.go
@@ -1,19 +1,44 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"strings"
+import "fmt"
+
+//Ring is info of ring
+type Ring struct {
+	a, b, h int
+}
+
+//RingPoiter point to ring
+type RingPoiter struct {
+	value   *int
+	pointer *Ring
+}
+
+var (
+	n       int
+	ring    [4]Ring
+	pointer [8]RingPoiter
 )
 
 func main() {
-	in := bufio.NewReader(os.Stdin)
+	fmt.Scan(&n)
+	for i := 0; i < n; i++ {
+		pointer[i*2].pointer = &ring[i]
+		pointer[i*2].value = &ring[i].a
 
-	ss, _ := in.ReadString('.')
-
-	ls := strings.Split(ss, string('\n'))
-	for i := 0; i < len(ls); i++ {
-		fmt.Println(ls[i])
+		pointer[i*2+1].pointer = &ring[i]
+		pointer[i*2+1].value = &ring[i].b
 	}
+
+	*pointer[0].value = 1
+	pointer[0], pointer[7] = Swap(pointer[0], pointer[7])
+
+	for i := 0; i < n*2; i++ {
+		fmt.Println(pointer[i].pointer)
+	}
+
+}
+
+//Swap ...
+func Swap(a, b RingPoiter) (RingPoiter, RingPoiter) {
+	return b, a
 }
